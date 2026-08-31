b58f5cbf620474ff558f1fbaeca5ae21bf3a88b9
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 19:22:09 +0530

    feat: solve Jump Game II using Greedy

---
+++ DP/13_jump_game_II.py
@@ -0,0 +1,20 @@
+def jump(nums):
+    jumps = 0
+    left = 0
+    right = 0
+
+    while right < len(nums) - 1:
+        farthest = 0
+
+        for i in range(left, right + 1):
+            farthest = max(farthest, i + nums[i])
+
+        left = right + 1
+        right = farthest
+        jumps += 1
+
+    return jumps
+
+
+nums = [2, 3, 1, 1, 4]
+print(jump(nums))
