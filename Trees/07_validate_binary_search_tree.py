48d5370406354cfa4714a0dde4f5866b2fc3c524
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 18:40:56 +0530

    feat: solve Validate Binary Search Tree using DFS

---
+++ Trees/07_validate_binary_search_tree.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_valid_bst(root):
+    def dfs(node, low, high):
+        if not node:
+            return True
+
+        if not (low < node.val < high):
+            return False
+
+        return (dfs(node.left, low, node.val) and
+                dfs(node.right, node.val, high))
+
+    return dfs(root, float("-inf"), float("inf"))
