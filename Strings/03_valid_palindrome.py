caa51237a24bff46a64fc22c79b7df5694eeda36
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 12:57:49 +0530

    feat: solve Valid Palindrome using two pointers

---
+++ Strings/03_valid_palindrome.py
@@ -0,0 +1,21 @@
+def is_palindrome(s):
+    cleaned = ""
+
+    for ch in s:
+        if ch.isalnum():
+            cleaned += ch.lower()
+
+    left = 0
+    right = len(cleaned) - 1
+
+    while left < right:
+        if cleaned[left] != cleaned[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+print(is_palindrome("A man, a plan, a canal: Panama"))
+print(is_palindrome("race a car"))
