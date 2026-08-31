06d9b0fdafb8365f32d9c63de794439ea27e5935
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 19:21:36 +0530

    feat: solve Jump Game using Greedy DP

---
+++ DP/12_jump_game.py
@@ -0,0 +1,12 @@
+def can_jump(nums):
+    goal = len(nums) - 1
+
+    for i in range(len(nums) - 2, -1, -1):
+        if i + nums[i] >= goal:
+            goal = i
+
+    return goal == 0
+
+
+nums = [2, 3, 1, 1, 4]
+print(can_jump(nums))
