16b72ba7ded93f8d4557635f420fc23adb9d279a
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 19:15:01 +0530

    feat: solve Longest Increasing Subsequence using DP

---
+++ DP/06_longest_increasing_subsequence.py
@@ -0,0 +1,13 @@
+def length_of_lis(nums):
+    dp = [1] * len(nums)
+
+    for i in range(len(nums) - 1, -1, -1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                dp[i] = max(dp[i], 1 + dp[j])
+
+    return max(dp)
+
+
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+print(length_of_lis(nums))
