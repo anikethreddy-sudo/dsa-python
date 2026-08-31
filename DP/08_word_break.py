19c379a4e6fbb620eaa723962df6f1e76f886c1b
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 19:18:32 +0530

    feat: solve Word Break using DP

---
+++ DP/08_word_break.py
@@ -0,0 +1,18 @@
+def word_break(s, word_dict):
+    words = set(word_dict)
+    dp = [False] * (len(s) + 1)
+    dp[0] = True
+
+    for i in range(1, len(s) + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in words:
+                dp[i] = True
+                break
+
+    return dp[len(s)]
+
+
+s = "leetcode"
+word_dict = ["leet", "code"]
+
+print(word_break(s, word_dict))
