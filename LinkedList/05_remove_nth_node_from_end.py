7bb42a8cb1c4982ecda3f83677cdf16226d9d4a8
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 18:22:37 +0530

    feat: solve Remove Nth Node From End

---
+++ LinkedList/05_remove_nth_node_from_end.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def remove_nth_from_end(head, n):
+    dummy = ListNode(0)
+    dummy.next = head
+
+    slow = dummy
+    fast = dummy
+
+    for _ in range(n + 1):
+        fast = fast.next
+
+    while fast:
+        slow = slow.next
+        fast = fast.next
+
+    slow.next = slow.next.next
+
+    return dummy.next
