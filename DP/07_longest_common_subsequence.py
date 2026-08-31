33fb8d66799654a9895ea00a146dc94b07d5e06e
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 19:17:57 +0530

    feat: solve Longest Common Subsequence using DP

---
+++ DP/07_longest_common_subsequence.py
@@ -0,0 +1,20 @@
+def longest_common_subsequence(text1, text2):
+    rows = len(text1)
+    cols = len(text2)
+
+    dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+    for i in range(rows - 1, -1, -1):
+        for j in range(cols - 1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dp[i + 1][j + 1]
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+    return dp[0][0]
+
+
+text1 = "abcde"
+text2 = "ace"
+
+print(longest_common_subsequence(text1, text2))
