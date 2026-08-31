68219c415d7ee975848557e265a101ae5d92328c
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 18:38:49 +0530

    feat: solve Same Tree using DFS

---
+++ Trees/03_same_tree.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_same_tree(p, q):
+    if not p and not q:
+        return True
+
+    if not p or not q:
+        return False
+
+    if p.val != q.val:
+        return False
+
+    return (is_same_tree(p.left, q.left) and
+            is_same_tree(p.right, q.right))
