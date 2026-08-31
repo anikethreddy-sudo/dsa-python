97578d51a3df4658b383746fc1f1966ee4561ca4
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 19:26:15 +0530

    feat: solve Distinct Subsequences using DP

---
+++ DP/20_distinct_subsequences.py
@@ -0,0 +1,23 @@
+def num_distinct(s, t):
+    rows = len(s)
+    cols = len(t)
+
+    dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+    for i in range(rows + 1):
+        dp[i][cols] = 1
+
+    for i in range(rows - 1, -1, -1):
+        for j in range(cols - 1, -1, -1):
+            dp[i][j] = dp[i + 1][j]
+
+            if s[i] == t[j]:
+                dp[i][j] += dp[i + 1][j + 1]
+
+    return dp[0][0]
+
+
+s = "rabbbit"
+t = "rabbit"
+
+print(num_distinct(s, t))
