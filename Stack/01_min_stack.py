24e4360ae9d4eef4fa4bdc708e21cb1f8ce045cf
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 18:28:35 +0530

    feat: solve Min Stack

---
+++ Stack/01_min_stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def get_min(self):
+        return self.min_stack[-1]
+
+
+s = MinStack()
+s.push(5)
+s.push(2)
+s.push(7)
+print(s.get_min())   # 2
