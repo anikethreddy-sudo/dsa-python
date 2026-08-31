fb2bf1e86cae13c62913f181bfa7a7847b4ec945
anikethreddy-sudo <devanikethreddy07@gmail.com>
2026-08-31 18:39:56 +0530

    feat: solve Lowest Common Ancestor of BST

---
+++ Trees/05_lowest_common_ancestor_of_bst.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def lowest_common_ancestor(root, p, q):
+    while root:
+        if p.val < root.val and q.val < root.val:
+            root = root.left
+        elif p.val > root.val and q.val > root.val:
+            root = root.right
+        else:
+            return root
